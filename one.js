97ceb2df9a7b4ec41aa7926c7aad32d952890db7
Addisalem Demis <142113805+mrsalvator77@users.noreply.github.com>
2024-09-04 06:52:48 -0900

    pomodoro

---
+++ one.js
@@ -0,0 +1,15 @@
+const strtBtn=document.getElementById("str")
+const stopBtn=document.getElementById("stp")
+const resetBtn=document.getElementById("rst")
+
+const minute=document.getElementById("min").value;
+const seconds=document.getElementById("sec").value;
+
+
+strtBtn.onclick("click",function (){
+for(let i=1;i<=59;i++){
+
+}
+})
+
+
